25c8078262fe12b438be69ea0f966f23db8f3696
Manjunath Naik <105489410+Manjunath2731@users.noreply.github.com>
2023-11-02 11:56:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,12 @@ const dotenv = require("dotenv").config();
 connectDb();
 const app = express();
 
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
